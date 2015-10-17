e11efea86686fcc73f53661016f24dbf66087314
kimeberz <kimeberz@gmail.com>
2015-10-17 09:19:15 -0700

    fixed some things

--- public/main.js
+++ public/main.js
@@ -1,10 +1,19 @@
+// $(function() {
+//   var FADE_TIME = 150; // ms
+//   var TYPING_TIMER_LENGTH = 400; // ms
+//   var COLORS = [
+//     '#e21400', '#91580f', '#f8a700', '#f78b00',
+//     '#58dc00', '#287b00', '#a8f07a', '#4ae8c4',
+//     '#3b88eb', '#3824aa', '#a700ff', '#d300e7'
+//   ];
+
 $(function() {
   var FADE_TIME = 150; // ms
   var TYPING_TIMER_LENGTH = 400; // ms
   var COLORS = [
-    '#e21400', '#91580f', '#f8a700', '#f78b00',
-    '#58dc00', '#287b00', '#a8f07a', '#4ae8c4',
-    '#3b88eb', '#3824aa', '#a700ff', '#d300e7'
+    '#00CDC1', '#00CDC1', '#00CDC1', '#00CDC1',
+    '#00CDC1', '#00CDC1', '#00CDC1', '#00CDC1',
+    '#00CDC1', '#00CDC1', '#00CDC1', '#00CDC1'
   ];
 
   // Initialize varibles
